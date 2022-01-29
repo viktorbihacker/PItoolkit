2108237595ea96091c74df5dc11a832175d1e1f8
Viktor Bihacker <viktor.bihacker@gmail.com>
2022-01-29 20:27:52 +0100

    password.py added

--- main.py
+++ main.py
@@ -1,42 +1,13 @@
-import random
 import sys
+import password
 
 
-def password_generator():
-    default_password_length = 12
-    maximum_password_length = 32
-    character_repository = {'special': [[33, 41], [43, 47], [58, 64], [91, 96], [123, 126]],
-                            'number': [48, 57], 'upper': [65, 90], 'lower': [97, 122]}
-    character_keys = ['special', 'number', 'upper', 'lower']
-    generated = ''
-    try:
-        character_number = int(input(f'Enter password length({default_password_length}-{maximum_password_length}): '))
-    except ValueError:
-        print(f'Password length set to {default_password_length}.')
-        character_number = default_password_length
-    if character_number < default_password_length:
-        print(f'Password length set to {default_password_length}.')
-        character_number = default_password_length
-    if character_number > maximum_password_length:
-        print(f'Password length set to {maximum_password_length}.')
-        character_number = maximum_password_length
-    while character_number > 0:
-        current_pick = character_keys[(character_number % 4) - 1]
-        if current_pick == 'special':
-            current_special = random.randint(0, 3)
-            range_start = character_repository.get(current_pick)[current_special][0]
-            range_end = character_repository.get(current_pick)[current_special][1]
-        else:
-            range_start = character_repository.get(current_pick)[0]
-            range_end = character_repository.get(current_pick)[1]
-        generated += chr(random.randint(range_start, range_end))
-        character_number -= 1
-    password = ''.join(random.sample(generated, len(generated)))
-    print(f'Password: {password}')
+def generate_password():
+    password.generator()
 
 
 def main():
-    menu_items = ["Password generator", "Second function", "Third function"]
+    menu_items = ["Generate password", "Second function", "Third function"]
     while True:
         application_name = "PItoolkit"
         application_name_placeholder = len(application_name) * "-"
