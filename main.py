b9f372bcccb5c35e53ec4659d9c8d4ef9a493742
Viktor Bihacker <viktor.bihacker@gmail.com>
2022-01-29 19:46:00 +0100

    menu_items print

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def main():
     menu_items = ["First function","Second function","Third function"]
     while True:
-        for i in menu_items:
-            print(i)
+        menu_placeholder = int(max([len(item) for item in menu_items]) / 2) * "-"
+        menu_header = f'{menu_placeholder}PItoolkit{menu_placeholder}'
+        menu_footer = f"{menu_placeholder}---------{menu_placeholder}"
+        for menu_item_index, menu_item in enumerate(menu_items):
+            if menu_item_index == 0:
+                print(menu_header)
+            print(f'{menu_item_index + 1}. {menu_item}')
+            if menu_item_index == len(menu_items) - 1:
+                print(menu_footer)
         quit()
 
 
