57bace079ead57783cf44f4fc7e674a00b76baf4
Viktor Bihacker <viktor.bihacker@gmail.com>
2022-01-29 20:45:13 +0100

    code reformat

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def main():
         application_name = "PItoolkit"
         application_name_placeholder = len(application_name) * "-"
         menu_title_placeholder = int(max([len(item) for item in menu_items]) / 2) * "-"
-        menu_header = f'{menu_title_placeholder}{application_name}{menu_title_placeholder}'
+        menu_header = f"{menu_title_placeholder}{application_name}{menu_title_placeholder}"
         menu_footer = f"{menu_title_placeholder}{application_name_placeholder}{menu_title_placeholder}"
         for menu_item_index, menu_item in enumerate(menu_items):
             if menu_item_index == 0:
                 print(menu_header)
-            print(f'{menu_item_index + 1}. {menu_item}')
+            print(f"{menu_item_index + 1}. {menu_item}")
             if menu_item_index == len(menu_items) - 1:
                 print(menu_footer)
         selection = input("Enter selection: ")
@@ -33,5 +33,5 @@ def main():
             quit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
