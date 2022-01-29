073ec7ea1fd46587f19f88073627b3d787b231fb
Viktor Bihacker <viktor.bihacker@gmail.com>
2022-01-29 20:18:55 +0100

    password generator method

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import sys
 
 
@@ -19,6 +20,19 @@ def password_generator():
     if character_number > maximum_password_length:
         print(f'Password length set to {maximum_password_length}.')
         character_number = maximum_password_length
+    while character_number > 0:
+        current_pick = character_keys[(character_number % 4) - 1]
+        if current_pick == 'special':
+            current_special = random.randint(0, 3)
+            range_start = character_repository.get(current_pick)[current_special][0]
+            range_end = character_repository.get(current_pick)[current_special][1]
+        else:
+            range_start = character_repository.get(current_pick)[0]
+            range_end = character_repository.get(current_pick)[1]
+        generated += chr(random.randint(range_start, range_end))
+        character_number -= 1
+    password = ''.join(random.sample(generated, len(generated)))
+    print(f'password: {password}')
 
 
 def main():
