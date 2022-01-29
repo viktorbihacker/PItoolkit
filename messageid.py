332865de360c3119ca084e646a6652480c4b3528
Viktor Bihacker <viktor.bihacker@gmail.com>
2022-01-29 22:30:40 +0100

    filter messageids

--- messageid.py
+++ messageid.py
@@ -4,3 +4,14 @@ def extract():
             text = file.readlines()
     except FileNotFoundError:
         quit("Please place an input file (messageid.input) in the root directory with the text...")
+
+    ids = []
+    duplicate_removed = []
+    for line in text:
+        for word in line.replace("\n", "").split(" "):
+            hyphen_number = 0
+            for letter in word:
+                if letter == "-":
+                    hyphen_number += 1
+            if hyphen_number == 4:
+                ids.append(word)
