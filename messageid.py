1d1db7a18fd0646c81ca4412269196f0fc3308c2
Viktor Bihacker <viktor.bihacker@gmail.com>
2022-01-29 22:29:53 +0100

    open input file

--- messageid.py
+++ messageid.py
@@ -1,2 +1,6 @@
 def extract():
-    pass
+    try:
+        with open("./messageid.input", "r") as file:
+            text = file.readlines()
+    except FileNotFoundError:
+        quit("Please place an input file (messageid.input) in the root directory with the text...")
