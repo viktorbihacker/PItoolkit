80e92b9270410819bf462ec04c846a917a5357aa
Viktor Bihacker <viktor.bihacker@gmail.com>
2022-02-04 22:21:40 +0100

    get token

--- mulesoft.py
+++ mulesoft.py
@@ -1,4 +1,18 @@
+import requests, json
+
+
 def catalog():
-    # get token with user credentials
-    username = input('Username: ')
-    password = input('Password: ')
+    # get credentials
+    username = input("Username: ")
+    password = input("Password: ")
+
+    # get token
+    url = 'https://anypoint.mulesoft.com/accounts/login'
+    payload = f'username={username}&password={password}'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    token_response = requests.post(url, headers=headers, data=payload)
+    token = json.loads(token_response.text).get('access_token')
+
+
+if __name__ == '__main__':
+    catalog()
