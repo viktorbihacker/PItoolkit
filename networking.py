64d1af9457a0ce9bb83eadf397a55a61d15f39d2
Viktor Bihacker <viktor.bihacker@agcocorp.com>
2022-04-27 21:17:26 +0200

    traceroute function

--- networking.py
+++ networking.py
@@ -11,7 +11,9 @@ def ping():
 
 
 def traceroute():
-    pass
+    host = input(str("Host: "))
+    command = ['tracert', host] if system_info() == 'windows' else ['traceroute', host]
+    return call_command(command)
 
 
 def system_info():
