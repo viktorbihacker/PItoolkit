7d8c8b3e347564c10f817bb4042a77c4dd1c01b8
Viktor Bihacker <viktor.bihacker@agcocorp.com>
2022-04-27 20:45:59 +0200

    ping function

--- networking.py
+++ networking.py
@@ -1,2 +1,10 @@
+import platform
+import subprocess
+
+
 def ping():
-    pass
+    host = input(str("Host: "))
+    packet_number = input(str("Number of packets (default=4): "))
+    param = '-n' if platform.system().lower() == 'windows' else '-c'
+    command = ['ping', param, packet_number if packet_number != "" else "4", host]
+    return subprocess.call(command) == 0
