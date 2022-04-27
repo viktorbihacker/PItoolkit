dc49d027a922fd0ca12f97bd5cabab4314ca38dc
Viktor Bihacker <viktor.bihacker@agcocorp.com>
2022-04-27 21:05:04 +0200

    system_info function

--- networking.py
+++ networking.py
@@ -5,6 +5,14 @@ import subprocess
 def ping():
     host = input(str("Host: "))
     packet_number = input(str("Number of packets (default=4): "))
-    param = '-n' if platform.system().lower() == 'windows' else '-c'
+    param = '-n' if system_info() == 'windows' else '-c'
     command = ['ping', param, packet_number if packet_number != "" else "4", host]
     return subprocess.call(command) == 0
+
+
+def traceroute():
+    pass
+
+
+def system_info():
+    return platform.system().lower()
